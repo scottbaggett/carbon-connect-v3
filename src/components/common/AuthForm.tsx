1522e5589970a7b373961e6cc6b4b8b1f08a818b
costrings-frontend-dev <frontend@costrings.com>
2024-06-19 14:10:40 +0530

    feedbacks + webscrapper flow

--- src/components/common/AuthForm.tsx
+++ src/components/common/AuthForm.tsx
@@ -12,7 +12,7 @@ interface AuthFormProps {
 export default function AuthForm({ onSubmit }: AuthFormProps) {
   return (
     <>
-      <div className="cc-p-4 cc-flex-grow">
+      <div className="cc-p-4 cc-min-h-0 cc-flex-grow">
         <div className="cc-p-2 cc-rounded-md cc-bg-surface-surface_1 cc-inline-block cc-mb-5">
           <img src={UserPlus} alt="User Plus" className="cc-h-6 cc-w-6" />
         </div>
